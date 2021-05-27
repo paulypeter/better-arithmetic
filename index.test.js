913724af1eaf645209c454edd3494b0fee7e6a83
Peter Pauly <p.pauly@tu-bs.de>
2021-05-27 20:07:55 +0200

    adds unit tests (FAILING), v1.0.3

---
+++ index.test.js
@@ -0,0 +1,25 @@
+const betterArithmetic = require("./index");
+
+test("Returns correct value for two positive integers.", () => {
+    expect(betterArithmetic.add(10, 5)).toBe(15);
+    expect(betterArithmetic.multiply(10, 5)).toBe(50);
+    expect(betterArithmetic.div(10, 5)).toBe(2);
+    expect(betterArithmetic.modulo(10, 5)).toBe(0);
+
+    expect(betterArithmetic.add(33, 7)).toBe(40);
+    expect(betterArithmetic.multiply(33, 7)).toBe(231);
+    expect(betterArithmetic.div(33, 7)).toBe(4);
+    expect(betterArithmetic.modulo(33, 7)).toBe(5);
+});
+
+test("Returns correct value for one positive and one negative integer.", () => {
+    expect(betterArithmetic.add(10, -5)).toBe(5);
+    expect(betterArithmetic.multiply(10, -5)).toBe(-50);
+    expect(betterArithmetic.div(10, -5)).toBe(-2);
+    expect(betterArithmetic.modulo(10, -5)).toBe(0);
+
+    expect(betterArithmetic.add(33, -7)).toBe(26);
+    expect(betterArithmetic.multiply(33, -7)).toBe(-231);
+    expect(betterArithmetic.div(33, -7)).toBe(-5);
+    expect(betterArithmetic.modulo(33, -7)).toBe(-2);
+});
