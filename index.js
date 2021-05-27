9459b30cb0c8cae50fd3e29d4ec0a5c8f571d93d
Peter Pauly <p.pauly@tu-bs.de>
2021-05-27 19:05:25 +0200

    adds square, multiply, add, div

--- index.js
+++ index.js
@@ -5,10 +5,13 @@
  *
  */
 exports.square = function(n) {
-	if typeof n === "number" {
+	if (typeof n === "number") {
+		if (n < 0) {
+			return this.square(-n);
+		}
 		let res = 0;
 		let add = 1;
-		for (let i = 1; i < n; i++) {
+		for (let i = 0; i < n; i++) {
 			res += add;
 			add += 2;
 		}
@@ -17,3 +20,87 @@ exports.square = function(n) {
 		throw 'input is not a number!'
 	}
 }
+
+/*
+ * \brief multiplies two numbers
+ *
+ * \params a, b integer numbers
+ */
+exports.multiply = function(a, b) {
+	if (typeof a === typeof b && typeof a === "number") {
+		if (a > b) {
+			let c = a;
+			a = b;
+			b = c;
+		}
+		if (b < 0) {
+			return this.multiply(this.sub(0, b), this.sub(0, a));
+		} else if (a < 0) {
+			return this.sub(0, this.multiply(this.sub(0, a), b))
+		} else if (a == 0) {
+			return 0;
+		} else if (a == 1) {
+			return b;
+		} else {
+			return this.add(this.square(a), this.multiply(a, this.sub(b, a)));
+		}
+	} else {
+		throw 'Input needs to be two numbers.'
+	}
+}
+
+/*
+ * \brief integer division of two numbers
+ *
+ * \params a, b integer numbers
+ */
+exports.div = function(a, b) {
+	if (typeof a === typeof b && typeof a === "number") {
+		let i = 0;
+		prod = this.multiply(i, b);
+		while (prod <= a) {
+			i = this.add(i, 1);
+			prod = this.multiply(i, b);
+		}
+		return this.sub(i, 1);
+	} else {
+		throw 'Input needs to be two numbers.'
+	}
+}
+
+/*
+ * \brief adds two numbers
+ *
+ * \params a, b integer numbers
+ */
+exports.add = function(a, b) {
+	if (typeof a === typeof b && typeof a === "number") {
+		// return this.div(this.sub(this.square(a), this.square(b)), this.sub(a, b))
+		if (a > b) {
+			let c = a;
+			a = b;
+			b = c;
+		}
+		if (b < 0) {
+			return this.sub(0, this.add(this.sub(0, a), this.sub(0, b)));
+		} else if (a < 0) {
+			return this.sub(b, this.sub(0, a));
+		} else if (a == 0) {
+			return b;
+		} else {
+			while (a > 0) {
+				a = this.sub(a, 1);
+				b = this.sub(b, this.sub(0, 1));
+			}
+			return b;
+		}
+	} else {
+		throw 'Input needs to be two numbers.'
+	}
+}
+
+exports.sub = function(a, b) {
+	return a - b;
+}
+
+console.log(this.div(100, 3))
